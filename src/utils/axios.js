690b9f4eee0a8b78ae0264307b1301ce245469e4
Nishthaa Jain <nishthaa2003@gmail.com>
2024-12-06 14:07:19 +0530

    baseurl

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://finance-tracker-backend-orpin.vercel.app/',
+    baseURL: 'https://finance-tracker-backend-orpin.vercel.app',
     // baseURL: 'http://127.0.0.1:4444',
     // baseURL:'http://localhost:4444',
     withCredentials: true
